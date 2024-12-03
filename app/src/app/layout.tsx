ceb5cabb9f78c5474f559c64b51354e43f5b90ca
Mouzinho Raimundo <mouzinho.raimundo@bnpsolucoes.com.br>
2024-12-03 13:48:06 -0300

    initial setup

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "GoLedger Challenge",
+  description:
+    "Serviço de streming, com registro de artista, álbum, música e playlist.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} bg-zinc-900 text-zinc-50 antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
